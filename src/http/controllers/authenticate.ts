db167d43440df65f6ada38af2130a0633833ea3b
Otávio Augusto Chrispim De Paiva <otaviochrispim@gmail.com>
2023-07-27 14:45:25 -0300

    JWT

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,7 +1,5 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { z } from 'zod';
-import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository';
-import { AuthenticateService } from '@/services/authenticate';
 import { InvalidCredentialsError } from '@/services/errors/invalid-credential-erro';
 import { makeAuthenticateService } from '@/services/factories/make-authenticate-service';
 
@@ -19,7 +17,18 @@ export async function authenticate(
   try {
     const authenticateService = makeAuthenticateService();
 
-    await authenticateService.execute({ email, password });
+    const { user } = await authenticateService.execute({ email, password });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+
+    return reply.status(200).send({ token });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message });
@@ -27,6 +36,4 @@ export async function authenticate(
 
     throw err;
   }
-
-  return reply.status(200).send();
 }
